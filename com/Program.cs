4b52fbd461aa316537f7efeda4e28068166a612a
wqh2216 <wqh2216@163.com>
2017-10-16 23:13:47 +0800

    添加tree显示

--- com/Program.cs
+++ com/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace sf
 {
@@ -10,12 +11,19 @@ namespace sf
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            AllocConsole();
+            Console.WriteLine("123");
         }
     }
 }
