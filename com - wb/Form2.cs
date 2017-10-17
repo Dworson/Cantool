22f55ca38bde1314ccd145de070d9149e8be1e8b
watermelon <744636901@qq.com>
2017-10-17 12:09:36 +0800

    保存设定内容

---
+++ com - wb/Form2.cs
@@ -0,0 +1,275 @@
+using System;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace sf
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            CommPort com = CommPort.Instance;
+
+            int found = 0;
+            string[] portList = com.GetAvailablePorts();
+            for (int i=0; i<portList.Length; ++i)
+            {
+                string name = portList[i];
+                comboBox1.Items.Add(name);
+                if (name == Settings.Port.PortName)
+                    found = i;
+            }
+            if (portList.Length > 0)
+                comboBox1.SelectedIndex = found;
+
+            Int32[] baudRates = {
+                100,300,600,1200,2400,4800,9600,14400,19200,
+                38400,56000,57600,115200,128000,256000,0
+            };
+            found = 0;
+            for (int i=0; baudRates[i] != 0; ++i)
+            {
+                comboBox2.Items.Add(baudRates[i].ToString());
+                if (baudRates[i] == Settings.Port.BaudRate)
+                    found = i;
+            }
+            comboBox2.SelectedIndex = 12;
+
+            comboBox3.Items.Add("5");
+            comboBox3.Items.Add("6");
+            comboBox3.Items.Add("7");
+            comboBox3.Items.Add("8");
+            comboBox3.SelectedIndex = Settings.Port.DataBits - 5;
+
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                comboBox4.Items.Add(s);
+            }
+            comboBox4.SelectedIndex = (int)Settings.Port.Parity;
+
+            //停止位选择项
+            comboBox5.Items.Add("0");
+            comboBox5.Items.Add("1");
+            comboBox5.Items.Add("1.5");
+            comboBox5.Items.Add("2");
+            comboBox5.SelectedIndex = 1;//默认为1
+
+            foreach (string s in Enum.GetNames(typeof(Handshake)))
+            {
+                comboBox6.Items.Add(s);
+            }
+            comboBox6.SelectedIndex = (int)Settings.Port.Handshake;
+
+            switch (Settings.Option.AppendToSend)
+            {
+                case Settings.Option.AppendType.AppendNothing:
+                    radioButton1.Checked = true;
+                    break;
+                case Settings.Option.AppendType.AppendCR:
+                    radioButton2.Checked = true;
+                    break;
+                case Settings.Option.AppendType.AppendLF:
+                    radioButton3.Checked = true;
+                    break;
+                case Settings.Option.AppendType.AppendCRLF:
+                    radioButton4.Checked = true;
+                    break;
+            }
+
+            checkBox1.Checked = Settings.Option.HexOutput;
+            checkBox2.Checked = Settings.Option.MonoFont;
+            checkBox3.Checked = Settings.Option.LocalEcho;
+            checkBox4.Checked = Settings.Option.StayOnTop;
+			checkBox5.Checked = Settings.Option.FilterUseCase;
+
+			textBox1.Text = Settings.Option.LogFileName;
+		}
+
+		// OK
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Settings.Port.PortName = comboBox1.Text;
+			Settings.Port.BaudRate = Int32.Parse(comboBox2.Text);
+			Settings.Port.DataBits = comboBox3.SelectedIndex + 5;
+			Settings.Port.Parity = (Parity)comboBox4.SelectedIndex;
+			Settings.Port.StopBits = (StopBits)comboBox5.SelectedIndex;
+			Settings.Port.Handshake = (Handshake)comboBox6.SelectedIndex;
+
+			if (radioButton2.Checked)
+				Settings.Option.AppendToSend = Settings.Option.AppendType.AppendCR;
+			else if (radioButton3.Checked)
+				Settings.Option.AppendToSend = Settings.Option.AppendType.AppendLF;
+			else if (radioButton4.Checked)
+				Settings.Option.AppendToSend = Settings.Option.AppendType.AppendCRLF;
+			else
+				Settings.Option.AppendToSend = Settings.Option.AppendType.AppendNothing;
+
+			Settings.Option.HexOutput = checkBox1.Checked;
+			Settings.Option.MonoFont = checkBox2.Checked;
+			Settings.Option.LocalEcho = checkBox3.Checked;
+			Settings.Option.StayOnTop = checkBox4.Checked;
+			Settings.Option.FilterUseCase = checkBox5.Checked;
+
+			Settings.Option.LogFileName = textBox1.Text;
+
+			CommPort com = CommPort.Instance;
+			com.Open();
+
+			Settings.Write();
+
+			Close();
+		}
+
+		// Cancel
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+		private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog1 = new OpenFileDialog();
+
+            fileDialog1.Title = "Open config";
+            fileDialog1.Filter = "config files (*.txt)|*.txt|All files (*.*)|*.*";
+            fileDialog1.FilterIndex = 1;
+            fileDialog1.RestoreDirectory = true;
+
+            if (fileDialog1.ShowDialog() == DialogResult.OK)
+            {
+				//声明一个文件流
+                FileStream fs = new FileStream(fileDialog1.FileName, FileMode.Open, FileAccess.Read, FileShare.None);
+                //创建读取器
+                StreamReader sr = new StreamReader(fs);
+
+                comboBox1.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox2.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox3.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox4.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox5.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox5.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+
+                radioButton1.Checked = Convert.ToBoolean(sr.ReadLine());
+                radioButton2.Checked = Convert.ToBoolean(sr.ReadLine());
+                radioButton3.Checked = Convert.ToBoolean(sr.ReadLine());
+                radioButton4.Checked = Convert.ToBoolean(sr.ReadLine());
+
+                checkBox1.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox2.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox3.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox4.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox5.Checked = Convert.ToBoolean(sr.ReadLine());
+
+                sr.Close();
+                fs.Close();
+			}
+            else
+            {
+				
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //保存设定内容
+            int portIndex = comboBox1.SelectedIndex;
+            int baudIndex = comboBox2.SelectedIndex;
+            int dataIndex = comboBox3.SelectedIndex;
+            int parityIndex = comboBox4.SelectedIndex;
+            int stopIndex = comboBox5.SelectedIndex;
+            int hardwareIndex = comboBox6.SelectedIndex;
+
+            bool nothingchecked = radioButton1.Checked;
+            bool CRchecked = radioButton2.Checked;
+            bool LRchecked = radioButton3.Checked;
+            bool CRLFchecked = radioButton4.Checked;
+
+            bool hexchecked = checkBox1.Checked;
+            bool Monospacedchecked= checkBox2.Checked;
+            bool localchecked = checkBox3.Checked;
+            bool staychecked = checkBox4.Checked;
+            bool filterchecked = checkBox5.Checked;
+
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\config.txt", false);
+            sw.WriteLine(portIndex);
+            sw.WriteLine(baudIndex);
+            sw.WriteLine(dataIndex);
+            sw.WriteLine(parityIndex);
+            sw.WriteLine(stopIndex);
+            sw.WriteLine(hardwareIndex);
+
+            sw.WriteLine(nothingchecked);
+            sw.WriteLine(CRchecked);
+            sw.WriteLine(LRchecked);
+            sw.WriteLine(CRLFchecked);
+
+            sw.WriteLine(hexchecked);
+            sw.WriteLine(Monospacedchecked);
+            sw.WriteLine(localchecked);
+            sw.WriteLine(staychecked);
+            sw.WriteLine(filterchecked);
+
+            sw.Close();
+            MessageBox.Show("保存成功！", "Save"); 
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string setting = Application.StartupPath + "\\config.txt";
+            if (File.Exists(setting))
+            {
+                //读取设定文件
+                StreamReader sr = new StreamReader(setting);
+                comboBox1.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox2.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox3.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox4.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox5.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+                comboBox5.SelectedIndex = Convert.ToInt32(sr.ReadLine());
+
+                radioButton1.Checked = Convert.ToBoolean(sr.ReadLine());
+                radioButton2.Checked = Convert.ToBoolean(sr.ReadLine());
+                radioButton3.Checked = Convert.ToBoolean(sr.ReadLine());
+                radioButton4.Checked = Convert.ToBoolean(sr.ReadLine());
+
+                checkBox1.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox2.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox3.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox4.Checked = Convert.ToBoolean(sr.ReadLine());
+                checkBox5.Checked = Convert.ToBoolean(sr.ReadLine());
+
+                sr.Close();
+
+                Settings.Port.PortName = comboBox1.Text;
+                Settings.Port.BaudRate = Int32.Parse(comboBox2.Text);
+                Settings.Port.DataBits = comboBox3.SelectedIndex + 5;
+                Settings.Port.Parity = (Parity)comboBox4.SelectedIndex;
+                Settings.Port.StopBits = (StopBits)comboBox5.SelectedIndex;
+                Settings.Port.Handshake = (Handshake)comboBox6.SelectedIndex;
+
+                if (radioButton2.Checked)
+                    Settings.Option.AppendToSend = Settings.Option.AppendType.AppendCR;
+                else if (radioButton3.Checked)
+                    Settings.Option.AppendToSend = Settings.Option.AppendType.AppendLF;
+                else if (radioButton4.Checked)
+                    Settings.Option.AppendToSend = Settings.Option.AppendType.AppendCRLF;
+                else
+                    Settings.Option.AppendToSend = Settings.Option.AppendType.AppendNothing;
+
+                Settings.Option.HexOutput = checkBox1.Checked;
+                Settings.Option.MonoFont = checkBox2.Checked;
+                Settings.Option.LocalEcho = checkBox3.Checked;
+                Settings.Option.StayOnTop = checkBox4.Checked;
+                Settings.Option.FilterUseCase = checkBox5.Checked;      
+
+                CommPort com = CommPort.Instance;
+                com.Open();
+
+                Settings.Write();
+            }
+        }
+    }
+}
---
+++ com - wb/Form3.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace sf
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.zGraph1 = new ZhengJuyin.UI.ZGraph();
+            this.SuspendLayout();
+            // 
+            // zGraph1
+            // 
+            this.zGraph1.BackColor = System.Drawing.Color.White;
+            this.zGraph1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.zGraph1.Location = new System.Drawing.Point(9, 9);
+            this.zGraph1.m_backColorH = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            this.zGraph1.m_backColorL = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_BigXYButtonForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_ControlButtonBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_ControlButtonForeColorH = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.zGraph1.m_ControlButtonForeColorL = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.zGraph1.m_ControlItemBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateLineColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateStringColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_coordinateStringTitleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_DirectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.zGraph1.m_DirectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_fXBeginSYS = 0F;
+            this.zGraph1.m_fXEndSYS = 60F;
+            this.zGraph1.m_fYBeginSYS = 0F;
+            this.zGraph1.m_fYEndSYS = 1F;
+            this.zGraph1.m_GraphBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_iLineShowColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.zGraph1.m_iLineShowColorAlpha = 100;
+            this.zGraph1.m_SySnameX = "X轴坐标";
+            this.zGraph1.m_SySnameY = "Y轴坐标";
+            this.zGraph1.m_SyStitle = "波形显示";
+            this.zGraph1.m_titleBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.zGraph1.m_titleColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.zGraph1.m_titlePosition = 0.4F;
+            this.zGraph1.m_titleSize = 14;
+            this.zGraph1.Margin = new System.Windows.Forms.Padding(0);
+            this.zGraph1.MinimumSize = new System.Drawing.Size(390, 270);
+            this.zGraph1.Name = "zGraph1";
+            this.zGraph1.Size = new System.Drawing.Size(686, 485);
+            this.zGraph1.TabIndex = 0;
+            this.zGraph1.Load += new System.EventHandler(this.zGraph1_Load);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 503);
+            this.Controls.Add(this.zGraph1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZhengJuyin.UI.ZGraph zGraph1;
+    }
+}
