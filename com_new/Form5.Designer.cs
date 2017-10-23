9ef0b871d1c6d244268b206bd5d8f808aeda5641
wqh2216 <wqh2216@163.com>
2017-10-23 11:26:29 +0800

    完善树型显示

---
+++ com_new/Form5.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace sf
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Time = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Chn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Dir = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Dlc = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Time,
+            this.Chn,
+            this.ID,
+            this.Name,
+            this.Dir,
+            this.Dlc,
+            this.Date});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(660, 363);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Time
+            // 
+            this.Time.Text = "Time";
+            this.Time.Width = 300;
+            // 
+            // Chn
+            // 
+            this.Chn.Text = "Chn";
+            this.Chn.Width = 30;
+            // 
+            // ID
+            // 
+            this.ID.Text = "ID";
+            this.ID.Width = 120;
+            // 
+            // Name
+            // 
+            this.Name.Text = "Name";
+            this.Name.Width = 120;
+            // 
+            // Dir
+            // 
+            this.Dir.Text = "Dir";
+            this.Dir.Width = 30;
+            // 
+            // Dlc
+            // 
+            this.Dlc.Text = "Dlc";
+            this.Dlc.Width = 30;
+            // 
+            // Date
+            // 
+            this.Date.Text = "Date";
+            this.Date.Width = 180;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 363);
+            this.Controls.Add(this.listView1);
+        
+            this.Text = "原始数据";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Time;
+        private System.Windows.Forms.ColumnHeader Chn;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader Dir;
+        private System.Windows.Forms.ColumnHeader Dlc;
+        private System.Windows.Forms.ColumnHeader Date;
+    }
+}
